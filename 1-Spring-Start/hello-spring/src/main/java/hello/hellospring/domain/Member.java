916a7e1ee723b0e84e245355f4be2f98624b9d3f
gahyeon <1000ji1@naver.com>
2023-12-17 00:44:45 +0900

    javax -> jakarta 오류 수정

--- 1-Spring-Start/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ 1-Spring-Start/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,6 +1,6 @@
 package hello.hellospring.domain;
 
-import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,18 +8,10 @@ import org.springframework.data.annotation.Id;
 
 @Entity
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @jakarta.persistence.Id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
@@ -27,5 +19,13 @@ public class Member {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
