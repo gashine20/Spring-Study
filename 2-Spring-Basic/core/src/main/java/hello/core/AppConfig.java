d5e24766d7c6f2dbbc4c0f6df37873346c9925d3
gahyeon <1000ji1@naver.com>
2023-12-21 15:45:15 +0900

    스프링 컨테이너에 등록 @Configuration, @Bean

--- 2-Spring-Basic/core/src/main/java/hello/core/AppConfig.java
+++ 2-Spring-Basic/core/src/main/java/hello/core/AppConfig.java
@@ -9,21 +9,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 구성 영역
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private static MemberRepository memberRepository() { // 역할
+    @Bean
+    public static MemberRepository memberRepository() { // 역할
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){ // 역할
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
