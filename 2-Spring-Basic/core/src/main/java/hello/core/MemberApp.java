ae4fed35df7dbac1b0814d47e9ea1774d0d7e924
gahyeon <1000ji1@naver.com>
2023-12-21 15:45:36 +0900

    스프링 컨테이너를 사용하여 의존성 주입

--- 2-Spring-Basic/core/src/main/java/hello/core/MemberApp.java
+++ 2-Spring-Basic/core/src/main/java/hello/core/MemberApp.java
@@ -3,12 +3,21 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // AppConfig에 있는 것들을 스프링 컨테이너에 넣어서 다 관리해줌
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // AppConfig에서 "memberService" 메소드를 가지고 와서 MemberService 타입으로 리턴
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
