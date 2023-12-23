4358597f838d5aa249043210e716b8607dc9efa1
gahyeon <1000ji1@naver.com>
2023-12-23 16:54:20 +0900

    객체 생성하고 같은 객체인지 확인

--- 2-Spring-Basic/core/src/test/java/hello/core/singleton/SingletonTest.java
+++ 2-Spring-Basic/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -1,8 +1,9 @@
 package hello.core.singleton;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -24,6 +25,23 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2);
 
         // memberService1 != memberService2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        // 참조값이 다른 것을 확인
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        // memberService1 != memberService2
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
+    
+
+
 }
